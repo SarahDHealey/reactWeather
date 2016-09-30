fb0e275f97d679d71fa12aa68ef1988fc02ea36c
Sarah Healey <s.deane.healey@gmail.com>
2016-09-30 14:49:56 -0600

    refactored one component into two nested components

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,79 @@
 
+//first nested component
+//follows single responsibility principle
+//remember: every component needs to have a render method
+
+//there are two types of components
+//1 - presentational component renders something visible to the screen
+//or responds to user input
+//presentational components don't maintain state
+//they just render to the browser and/or call functions when the user interacts
+//2 - container components do maintain state.
+//It automatically renders its children when its state changes and the children rely on it
+//notice there is nothing inside the parent other than the children
+
+//we are going to pass a greeter component as a prop into the greeter form
+//define that function in greeter
+
+//GreeterMessage Component aka presentation component (child)
+var GreeterMessage = React.createClass({
+  render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+      </div>
+    );
+  }
+});
+
+//GreeterForm Component aka presentation component (child)
+var GreeterForm = React.createClass({
+  onFormSubmit: function(e){
+    e.preventDefault();
+
+    var updates = {};
+    var name = this.refs.name.value;
+    var message = this.refs.message.value;
+
+    if(name.length > 0) {
+      this.refs.name.value = '';
+      updates.name = name;
+    }
+
+    if(message.length > 0) {
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+    this.props.onNewData(updates);
+  },
+  render: function() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <div>
+          <input type="text" ref="name" placeholder="Enter name"/>
+        </div>
+        <div>
+          <textarea ref="message" placeholder="Enter message"></textarea>
+        </div>
+        <div>
+          <button>submit</button>
+        </div>
+      </form>
+    );
+  }
+});
+
+//3 things to do here:
+//1. set initial message state equal to whatever prop got passed in
+//ie this.props.message
+//inside handleNewName you are getting an updates object
+//pass the object directly to setState
+//finally access this.state.message
+//Greeter Component aka container component (parent)
 var Greeter = React.createClass({
   getDefaultProps: function() {
     return {
@@ -8,42 +83,34 @@ var Greeter = React.createClass({
   },
   getInitialState: function() {
     return {
-      name: this.props.name
+      name: this.props.name,
+      message: this.props.message
     };
   },
-  onButtonClick: function(e) {
-    e.preventDefault();
-    var nameRef = this.refs.name
-    var name = nameRef.value;
-    nameRef.value = '';
-
-    if (typeof name === "string" && name.length > 0) {
-      this.setState({
-        name: name
-      });
-    }
+  handleNewData: function(updates) {
+    this.setState(updates);
   },
   render: function() {
     var name = this.state.name;
-    var message = this.props.message;
+    var message = this.state.message;
 
+    //nest the GreeterMessage component
+    //when you have a parent component that is handling an event from a children
+    //you usually call the parent handle followed by the name you chose
+    //and then you call it in the child with on followed by the name you chose
+    //now you have a new prop. You can access it with this.props.onNewName
+    //all it does is call the handleNewName function that sets the state
     return (
       <div>
-        <h2>Hello, {name}</h2>
-        <h1>This is Your First React Component</h1>
-        <p>{message + ' tada!'}</p>
-        <form onSubmit={this.onButtonClick}>
-          <input type="text" ref="name"/>
-          <button>Set Name</button>
-        </form>
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewData = {this.handleNewData}/>
       </div>
     );
   }
 });
 var firstName = 'Sarah';
-var message = 'Bonjourno Princepessa'
 
 ReactDOM.render(
-  <Greeter name={firstName} message={message}/>,
+  <Greeter name={firstName}/>,
   document.getElementById('app')
 );
