86e66f3cbc6fd42437735126cfa3f489da44de60
Sarah Healey <s.deane.healey@gmail.com>
2016-09-30 14:55:33 -0600

    removed comments

--- public/app.jsx
+++ public/app.jsx
@@ -1,20 +1,4 @@
 
-//first nested component
-//follows single responsibility principle
-//remember: every component needs to have a render method
-
-//there are two types of components
-//1 - presentational component renders something visible to the screen
-//or responds to user input
-//presentational components don't maintain state
-//they just render to the browser and/or call functions when the user interacts
-//2 - container components do maintain state.
-//It automatically renders its children when its state changes and the children rely on it
-//notice there is nothing inside the parent other than the children
-
-//we are going to pass a greeter component as a prop into the greeter form
-//define that function in greeter
-
 //GreeterMessage Component aka presentation component (child)
 var GreeterMessage = React.createClass({
   render: function() {
@@ -34,7 +18,6 @@ var GreeterMessage = React.createClass({
 var GreeterForm = React.createClass({
   onFormSubmit: function(e){
     e.preventDefault();
-
     var updates = {};
     var name = this.refs.name.value;
     var message = this.refs.message.value;
@@ -43,7 +26,6 @@ var GreeterForm = React.createClass({
       this.refs.name.value = '';
       updates.name = name;
     }
-
     if(message.length > 0) {
       this.refs.message.value = '';
       updates.message = message;
@@ -67,12 +49,6 @@ var GreeterForm = React.createClass({
   }
 });
 
-//3 things to do here:
-//1. set initial message state equal to whatever prop got passed in
-//ie this.props.message
-//inside handleNewName you are getting an updates object
-//pass the object directly to setState
-//finally access this.state.message
 //Greeter Component aka container component (parent)
 var Greeter = React.createClass({
   getDefaultProps: function() {
@@ -94,12 +70,6 @@ var Greeter = React.createClass({
     var name = this.state.name;
     var message = this.state.message;
 
-    //nest the GreeterMessage component
-    //when you have a parent component that is handling an event from a children
-    //you usually call the parent handle followed by the name you chose
-    //and then you call it in the child with on followed by the name you chose
-    //now you have a new prop. You can access it with this.props.onNewName
-    //all it does is call the handleNewName function that sets the state
     return (
       <div>
         <GreeterMessage name={name} message={message}/>
