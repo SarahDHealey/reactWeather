13dcd7bc75778c7717b32ce1f67ac6bf8131e0ac
Sarah Healey <s.deane.healey@gmail.com>
2016-10-06 12:23:18 -0600

    updated server.js

--- server.js
+++ server.js
@@ -17,9 +17,8 @@ app.use(function(req, res, next){
 //call express.static
 //express.static is going to specify a folder name that we want to expose to the web server
 app.use(express.static('public'));
-
 //start the server
 //app.listen takes two arguments - the port and a function which will get called once the server is up
-app.listen(port, function(){
-  console.log('Express server is up on port ' + PORT);
+app.listen(port, function() {
+  console.log('Express server is up on port ' + port);
 })
