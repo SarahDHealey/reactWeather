65aad6e7ab80db9e109e6801a7b17acac8f05497
Sarah Healey <s.deane.healey@gmail.com>
2016-10-08 21:16:30 -0600

    environment variables not in place properly

--- app/api/OpenWeatherMap.jsx
+++ app/api/OpenWeatherMap.jsx
@@ -1,7 +1,9 @@
 var axios = require('axios');
 
+const weatherAPIKey=process.env.OPENWEATHERMAPAPI_KEY || '04a4d31e83c89dd7d4c882619e7ef4c2';
+
 //generate the base url
-const OPEN_WEATHER_MAP_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPENWEATHERMAPAPI_KEY}&units=imperial`;
+const OPEN_WEATHER_MAP_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${weatherAPIKey}&units=imperial`;
 
 module.exports = {
   getTemp: function(location) {
